2f553eb956dd748ec077c2ae8d15b97bf1e54a55
Tiago Nunes <ntiagon@gmail.com>
2021-11-03 14:28:04 -0300

    first comment

---
+++ django/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    summary = models.CharField(max_length=255)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug])
+
